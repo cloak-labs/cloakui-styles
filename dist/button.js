b37c19b7b8bf5040f6ab0e3fd17ba989a07cb644
Kaelan Smith <kaelancsmith@gmail.com>
2024-05-27 08:48:02 -0700

    add component classes to button variants for easier global CSS overrides

--- dist/button.js
+++ dist/button.js
@@ -1,14 +1,14 @@
 import { cva } from "./cva";
 export const buttonStyles = cva({
-    base: "[text-shadow:none] inline-flex items-center justify-center whitespace-nowrap rounded-sm text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-focus disabled:pointer-events-none disabled:opacity-50",
+    base: "[text-shadow:none] inline-flex items-center justify-center whitespace-nowrap rounded-sm text-sm font-semibold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-focus disabled:pointer-events-none disabled:opacity-50 btn",
     variants: {
         variant: {
-            default: "bg-primary text-primary shadow-sm hover:bg-primary/90",
-            destructive: "bg-destructive text-destructive shadow-sm hover:bg-destructive/90",
-            outline: "text-root border border-root-dim bg-transparent shadow-sm hover:bg-root-dim hover:text-root-dim",
-            secondary: "bg-root-dim text-root-vivid shadow-sm border border-root-dim hover:bg-root-dim/80",
-            ghost: "hover:bg-root-dim hover:text-root-dim",
-            link: "text-link underline-offset-4 hover:underline",
+            default: "bg-primary text-primary shadow-sm hover:bg-primary/90 btn-default",
+            destructive: "bg-destructive text-destructive shadow-sm hover:bg-destructive/90 btn-destructive",
+            outline: "text-root border border-root-dim bg-transparent shadow-sm hover:bg-root-dim hover:text-root-dim btn-outline",
+            secondary: "bg-root-dim text-root-vivid shadow-sm border border-root-dim hover:bg-root-dim/80 btn-secondary",
+            ghost: "hover:bg-root-dim hover:text-root-dim btn-ghost",
+            link: "text-link underline-offset-4 hover:underline btn-link",
         },
         size: {
             default: "h-9 px-4 py-2",
