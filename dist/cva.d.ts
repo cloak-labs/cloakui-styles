e51691841cb9f9eeb4818392096b8ff966d15009
Kaelan Smith <kaelancsmith@gmail.com>
2024-09-19 12:22:11 -0700

    add cxDeep util

--- dist/cva.d.ts
+++ dist/cva.d.ts
@@ -1,5 +1,9 @@
 import { CX, type CVA } from "cva";
-import { Compose } from "./types";
+import { ClassObject, Compose } from "./types";
 export declare const cva: CVA;
 export declare const cx: CX;
 export declare const compose: Compose;
+/**
+ * A wrapper function around `cx` that handles merging className objects, where each key in each object references a classList (or a nested class object).
+ */
+export declare function cxDeep<T extends ClassObject = ClassObject>(...objects: T[]): T;
--- src/cva.ts
+++ src/cva.ts
@@ -1,8 +1,47 @@
 import { getApi } from "./config";
 import { CX, type CVA } from "cva";
-import { Compose } from "./types";
+import { ClassObject, ClassValue, Compose } from "./types";
 
 // We export wrappers around cva/cx/compose that act as getters for our user-configured cva/cx/compose functions
 export const cva: CVA = (props) => getApi().cva(props);
 export const cx: CX = (...inputs) => getApi().cx(...inputs);
 export const compose: Compose = (...args) => getApi().compose(...args);
+
+function isClassObject<T>(obj: any): obj is T {
+  return obj && typeof obj === "object" && !Array.isArray(obj);
+}
+
+/**
+ * A wrapper function around `cx` that handles merging className objects, where each key in each object references a classList (or a nested class object).
+ */
+export function cxDeep<T extends ClassObject = ClassObject>(
+  ...objects: T[]
+): T {
+  const merged: T = {} as T;
+
+  for (const obj of objects) {
+    for (const key in obj) {
+      if (isClassObject(obj[key])) {
+        if (merged[key] && isClassObject(merged[key])) {
+          merged[key] = cxDeep(merged[key] as T, obj[key] as T) as T[Extract<
+            keyof T,
+            string
+          >];
+        } else {
+          merged[key] = cxDeep(obj[key] as T) as T[Extract<keyof T, string>];
+        }
+      } else {
+        if (merged[key]) {
+          merged[key] = cx(
+            merged[key] as ClassValue,
+            obj[key] as ClassValue
+          ) as T[Extract<keyof T, string>];
+        } else {
+          merged[key] = obj[key];
+        }
+      }
+    }
+  }
+
+  return merged;
+}
