89b5cf7bb8d408a03128c11583cebc80d49e117b
Kaelan Smith <kaelancsmith@gmail.com>
2024-09-19 12:22:50 -0700

    add cxDeep util

--- dist/cva.js
+++ dist/cva.js
@@ -3,3 +3,33 @@ import { getApi } from "./config";
 export const cva = (props) => getApi().cva(props);
 export const cx = (...inputs) => getApi().cx(...inputs);
 export const compose = (...args) => getApi().compose(...args);
+function isClassObject(obj) {
+    return obj && typeof obj === "object" && !Array.isArray(obj);
+}
+/**
+ * A wrapper function around `cx` that handles merging className objects, where each key in each object references a classList (or a nested class object).
+ */
+export function cxDeep(...objects) {
+    const merged = {};
+    for (const obj of objects) {
+        for (const key in obj) {
+            if (isClassObject(obj[key])) {
+                if (merged[key] && isClassObject(merged[key])) {
+                    merged[key] = cxDeep(merged[key], obj[key]);
+                }
+                else {
+                    merged[key] = cxDeep(obj[key]);
+                }
+            }
+            else {
+                if (merged[key]) {
+                    merged[key] = cx(merged[key], obj[key]);
+                }
+                else {
+                    merged[key] = obj[key];
+                }
+            }
+        }
+    }
+    return merged;
+}
